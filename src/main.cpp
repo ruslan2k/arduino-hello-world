8c4523a7a396b4ebaff389ae031ad085a3cec122
Ruslan <ruslan@example.com>
2024-07-12 10:20:57 +0300

    Added DS1302

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,41 @@
 #include <Arduino.h>
+#include <DS1302.h>
 
-int led_pin=13;
+// Set the pins for the DS1302
+const int RST_PIN = 5; // Reset
+const int DAT_PIN = 6; // Data
+const int CLK_PIN = 7; // Clock
+
+// Create an instance of the DS1302 class
+DS1302 rtc(RST_PIN, DAT_PIN, CLK_PIN);
 
 void setup() {
-  int led_pin=13;
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+
+  rtc.writeProtect(false);
+  rtc.halt(false);
+
+  // rtc.time(Time(2024, 7, 10, 0, 0, 0, Time::kWednesday));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(led_pin, HIGH);
+  // Set the LED HIGH
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("HIGH");
+
+  // Wait for a second
   delay(1000);
-  digitalWrite(led_pin, LOW);
+
+  // Set the LED LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LOW");
+
   delay(10000);
+
+  Serial.print(rtc.time().min);
+  Serial.print(':');
+  Serial.print(rtc.time().sec);
+  Serial.println(' ');
+
 }
