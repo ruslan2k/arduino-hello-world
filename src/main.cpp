b0337d995b5bd7becde6d4d41750c2715d6ed23b
Ruslan <ruslan@example.com>
2024-08-17 12:56:56 +0300

    Added new lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,79 @@
 #include <Arduino.h>
+// Example sketch for interfacing with the DS1302 timekeeping chip.
+//
+// Copyright (c) 2009, Matt Sparks
+// All rights reserved.
+//
+// http://quadpoint.org/projects/arduino-ds1302
+#include <stdio.h>
 #include <DS1302.h>
 
-// Set the pins for the DS1302
-const int RST_PIN = 5; // Reset
-const int DAT_PIN = 6; // Data
-const int CLK_PIN = 7; // Clock
+namespace {
 
-// Create an instance of the DS1302 class
-DS1302 rtc(RST_PIN, DAT_PIN, CLK_PIN);
+// Set the appropriate digital I/O pin connections. These are the pin
+// assignments for the Arduino as well for as the DS1302 chip. See the DS1302
+// datasheet:
+//
+//   http://datasheets.maximintegrated.com/en/ds/DS1302.pdf
+const int kCePin   = 5;  // Chip Enable
+const int kIoPin   = 6;  // Input/Output
+const int kSclkPin = 7;  // Serial Clock
+
+// Create a DS1302 object.
+DS1302 rtc(kCePin, kIoPin, kSclkPin);
+
+String dayAsString(const Time::Day day) {
+  switch (day) {
+    case Time::kSunday: return "Sunday";
+    case Time::kMonday: return "Monday";
+    case Time::kTuesday: return "Tuesday";
+    case Time::kWednesday: return "Wednesday";
+    case Time::kThursday: return "Thursday";
+    case Time::kFriday: return "Friday";
+    case Time::kSaturday: return "Saturday";
+  }
+  return "(unknown day)";
+}
+
+void printTime() {
+  // Get the current time and date from the chip.
+  Time t = rtc.time();
+
+  // Name the day of the week.
+  const String day = dayAsString(t.day);
+
+  // Format the time and date and insert into the temporary buffer.
+  char buf[50];
+  snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
+           day.c_str(),
+           t.yr, t.mon, t.date,
+           t.hr, t.min, t.sec);
+
+  // Print the formatted string to serial so we can see the time.
+  Serial.println(buf);
+}
+
+}  // namespace
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
 
+  // Initialize a new chip by turning off write protection and clearing the
+  // clock halt flag. These methods needn't always be called. See the DS1302
+  // datasheet for details.
   rtc.writeProtect(false);
   rtc.halt(false);
 
-  // rtc.time(Time(2024, 7, 10, 0, 0, 0, Time::kWednesday));
+  // Make a new time object to set the date and time.
+  // Sunday, September 22, 2013 at 01:38:50.
+  Time t(2013, 9, 22, 1, 38, 50, Time::kSunday);
+
+  // Set the time and date on the chip.
+  rtc.time(t);
 }
 
+// Loop and print the time every second.
 void loop() {
-  // Set the LED HIGH
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("HIGH");
-
-  // Wait for a second
+  printTime();
   delay(1000);
-
-  // Set the LED LOW
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("LOW");
-
-  delay(10000);
-
-  Serial.print(rtc.time().min);
-  Serial.print(':');
-  Serial.print(rtc.time().sec);
-  Serial.println(' ');
-
 }
